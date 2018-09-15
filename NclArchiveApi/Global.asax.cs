bf3b68ba1f0bdb9e6bc2b091042732352c20830d
J Kapp <github@jezzer.me>
2018-09-15 11:13:01 +0100

    Configure SimpleInjector

--- NclArchiveApi/Global.asax.cs
+++ NclArchiveApi/Global.asax.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using DatabaseAccess.Repositories;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
 
 namespace NclArchiveApi
 {
@@ -11,6 +10,19 @@ namespace NclArchiveApi
     {
         protected void Application_Start()
         {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            container.Register<IClubRepository, ClubRepository>(Lifestyle.Singleton);
+            container.Register<ITeamRepository, TeamRepository>(Lifestyle.Singleton);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
